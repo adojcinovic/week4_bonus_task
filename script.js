26219685d4008ca87bbbb56db03725c376c45c99
adojcinovic <adojca@gmail.com>
2021-09-30 16:15:46 +0200

    email fix 2

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 const email = document.querySelector('.email')
 
 function checkEmail() {
-    emailRegex.test(email.firstElementChild.value) ? null : email.classList.add('show')
-    email.firstElementChild.value = ''
-    email.firstElementChild.placeholder = 'example@quantox.com'
+    if (emailRegex.test(email.firstElementChild.value)) {
+        email.classList.add('email')
+    } else {
+        email.classList.add('show')
+        email.firstElementChild.value = ''
+        email.firstElementChild.placeholder = 'example@quantox.com'
+    }
 }
 
 function checkInputs() {
     inputs.forEach((input) => {
         input.firstElementChild.value.length === 0 ? input.classList.add('show') : input.classList.remove('show')
+        checkEmail()
     })
 }
 
 button.addEventListener('click', () => {
-    checkEmail()
     checkInputs()
 })
